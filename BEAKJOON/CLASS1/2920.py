2dca3deba492d2abd82a2558ca4ceada56559146
hojeong <jhj20071@naver.com>
2021-07-24 19:46:01 +0900

    음계 문제 완료

--- BEAKJOON/CLASS1/2920.py
+++ BEAKJOON/CLASS1/2920.py
@@ -6,12 +6,14 @@ if num_list[0]=='1':
             ans='ascending'
         else:
             ans='mixed'
+            break
 elif num_list[0]=='8':
     for i in range(8,0,-1):
         if str(i)==num_list[8-i]:
             ans='descending'
         else:
             ans='mixed'
+            break
 else:
     ans='mixed'
-print(ans)     
+print(ans)      
