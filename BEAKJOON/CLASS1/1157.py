62518c213708d8d2396012784258c761a9231d25
hojeong <jhj20071@naver.com>
2021-07-24 20:53:41 +0900

    단어 공부 문제 완료

--- BEAKJOON/CLASS1/1157.py
+++ BEAKJOON/CLASS1/1157.py
@@ -1,11 +1,16 @@
-my_word=input()
-my_word=my_word.upper()
-cnt=0
-ans=''
-for i in range(len(my_word)):
-    if cnt<my_word.count(my_word[i]):
-        cnt=my_word.count(my_word[i])
-        ans=my_word[i]
-    elif cnt==my_word.count(my_word[i]) and ans!=my_word[i]:
-        ans='?'
-print(ans)        
+my_word=list(input().upper())
+ans_list=[]
+for i in range(65,91):
+    cnt=0
+    for j in my_word:
+        if chr(i)==j:
+            cnt+=1
+    ans_list.append(cnt)
+my_max=max(ans_list)
+my_count=ans_list.count(my_max)
+if my_count==1:
+    for k in range(26):
+        if ans_list[k]==my_max:
+            print(chr(k+65))
+else:
+    print('?')      
