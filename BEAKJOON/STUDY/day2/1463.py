d8407fac7b54d6246ec39436d6f091b947f0b163
hojeong <jhj20071@naver.com>
2021-08-02 22:37:25 +0900

    1로 만들기 해결 (모든 경우의 수)

--- BEAKJOON/STUDY/day2/1463.py
+++ BEAKJOON/STUDY/day2/1463.py
@@ -1,14 +1,21 @@
 #1로 만들기
 n=int(input())
+f_list=[n]
 cnt=0
-while n !=1:
-    if (n-1)%3==0 and (n//2)%2:
-        n-=1
-    elif n%3==0:
-        n//=3
-    elif n%2==0:
-        n//=2
-    else:
-        n-=1
-    cnt+=1
+if n==1:
+    print(0)
+else:
+    while 1:
+        cnt+=1
+        n_list=[]
+        for i in f_list:
+            if i%3==0:
+                n_list.append(i//3)
+            if i%2==0:
+                n_list.append(i//2)
+            n_list.append(i-1)
+        if 1 in n_list:
+            break
+        f_list=list(set(n_list))
+        
 print(cnt)
