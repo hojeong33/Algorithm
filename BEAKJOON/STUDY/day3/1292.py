1bc5aea7fd3cd4bb205d7ef8f759c485c2d72fc2
hojeong <jhj20071@naver.com>
2021-08-04 19:36:10 +0900

    쉽게 푸는 문제 추가 설명

--- BEAKJOON/STUDY/day3/1292.py
+++ BEAKJOON/STUDY/day3/1292.py
@@ -4,3 +4,23 @@ n_list=[]
 for i in range(1,46): # range(1,end)까지로 하면?
     n_list+=[i]*i
 print(sum(n_list[start-1:end]))
+
+
+# 입력값 제어??
+# cnt=0
+# flag=False
+# while 1:
+#     for i in range(1,1000):
+#         for j in range(i):
+#             cnt+=1
+#             if cnt==1000:
+#                 print(i)
+#                 flag=True
+#                 break
+#     if flag:
+#         break
+
+
+        
+
+    
