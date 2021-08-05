e010f504a04f8519d9ae7d5d7f29738a4d3a392f
hojeong <jhj20071@naver.com>
2021-08-05 23:54:11 +0900

    듣보잡 문제 완료

---
+++ BEAKJOON/STUDY/day4/1764.py
@@ -0,0 +1,38 @@
+#듣보잡
+N,M=map(int,input().split())
+N_list=[]
+M_list=[]
+for i in range(N):
+    N_list.append(input())
+for i in range(M):
+    M_list.append(input())
+
+ans=set(N_list)&set(M_list) # set 교집합
+print(len(ans))
+print(*sorted(ans),sep="\n") #정렬된 set 출력
+
+# 시간초과 
+
+# N,M=map(int,input().split())
+# pre_lsit=[]
+# ans_list=[]
+# for i in range(N+M):
+#     name=input()
+#     if not(name in pre_lsit):
+#         pre_lsit.append(name)
+#     else:
+#         ans_list.append(name)
+# print(len(ans_list))
+# print(*ans_list,sep='\n')
+
+# N,M=map(int,input().split())
+# N_list=[]
+# ans_list=[]
+# for i in range(N):
+#     N_list.append(input())
+# for i in range(M):
+#     name=input()
+#     if name in N_list:
+#         ans_list.append(name)
+# print(len(ans_list))
+# print(*ans_list,sep='\n')
