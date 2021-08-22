092cda3ba1897cb40ecb7ce87006fb401fadb178
hojeong <jhj20071@naver.com>
2021-08-23 02:37:50 +0900

    크로아티아 알파벳 문제 완료

--- BEAKJOON/STUDY/day2/2941.py
+++ BEAKJOON/STUDY/day2/2941.py
@@ -1,1 +1,8 @@
 #크로아티아 알파벳
+change=['c=','c-','dz=','d-','s=','z=','lj','nj']
+word=input()
+l=len(word)
+cnt=0
+for i in change:
+    cnt+=word.count(i)
+print(l-cnt) #길이에서 변경 알파벳 갯수 만큼 빼주면 크로아티아 알파벳 개수
