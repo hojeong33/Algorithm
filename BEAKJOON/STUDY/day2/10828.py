f1ef0dc0ff2aa23faabd77f1e7d91cb92d54210d
hojeong <jhj20071@naver.com>
2021-08-23 02:08:41 +0900

    스택 시간초과

--- BEAKJOON/STUDY/day2/10828.py
+++ BEAKJOON/STUDY/day2/10828.py
@@ -1,1 +1,21 @@
 #스택
+#출력해야되는 명령어 pop,size,empty,top
+N=int(input())
+stack=[]
+for i in range(N):
+    command=input()
+    if 'push' in command:
+        command=command[5:]
+        stack.append(command)
+    elif command=='pop' and stack:
+        print(stack.pop())
+    elif command=='size':
+        print(len(stack))
+    elif command=='empty' and stack:
+        print(0)
+    elif command=='empty' and len(stack)==0:
+        print(1)
+    elif command=='top'and stack:
+        print(stack[-1])
+    elif len(stack)==0:
+        print(-1)
