4b7e69e0052701f1f6955cf0765ea1e60ad8d5b1
hojeong <jhj20071@naver.com>
2021-08-23 03:13:35 +0900

    단어 정렬 PyPy로 실행했을 경우 시간초과 문제 해결

--- BEAKJOON/STUDY/day2/1181.py
+++ BEAKJOON/STUDY/day2/1181.py
@@ -1,1 +1,17 @@
 #단어 정렬
+
+N=int(input())
+words=[0]*N
+for i in range(N):
+    words[i]=input()
+words=list(set(words)) # 같은 단어가 여러번 입력된 경우에는 한번씩만 출력
+l=len(words)
+
+for i in range(l-1):
+    for j in range(i+1,l):
+        if len(words[i])>len(words[j]):
+            words[i],words[j]=words[j],words[i]
+        elif len(words[i])==len(words[j]):
+            if words[i]>words[j]:
+                words[i],words[j]=words[j],words[i]
+print(*words,sep='\n')
