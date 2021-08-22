30d02bb913b97f1ed2682688b07daa8dad665657
hojeong <jhj20071@naver.com>
2021-08-22 18:29:37 +0900

    나이순 정렬 - 시간초과

--- BEAKJOON/STUDY/day2/10814.py
+++ BEAKJOON/STUDY/day2/10814.py
@@ -1,1 +1,19 @@
 #나이순 정렬
+N=int(input())
+name=[]
+age=[]
+for i in range(N):
+    a,n=input().split()
+    name.append(n)
+    age.append(int(a))
+while age:
+    for i in range(len(age)):
+        num=age[0]
+        num_index=0
+        if num>age[i]:
+            num=age[i]
+            num_index=i
+    print('{} {}'.format(age[num_index],name[num_index]))
+    age.pop(num_index)
+    name.pop(num_index)
+
