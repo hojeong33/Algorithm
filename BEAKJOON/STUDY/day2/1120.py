37e46b15244956fa3f106fa0b9013538700ed14e
hojeong <jhj20071@naver.com>
2021-08-23 01:48:59 +0900

    문자열 문제 완료

--- BEAKJOON/STUDY/day2/1120.py
+++ BEAKJOON/STUDY/day2/1120.py
@@ -1,1 +1,19 @@
 #문자열
+def myDif(lst1,lst2): #lst1:A lst:B
+    n=len(lst1)+pass_zone #len(lst2)-(len(lst2)-len(lst1)-pass_zone)
+    result=0
+    for i in range(pass_zone,n):
+        if lst1[i-pass_zone]!=lst2[i]:
+            result+=1
+    return result
+
+A,B=input().split()
+a=len(A)
+b=len(B)
+min_cnt=a
+for i in range(b-a+1): # i일때 앞에 알파벳 추가 i개 뒤에 추가 길이 차이-i
+    pass_zone=i
+    temp=myDif(A,B)
+    if min_cnt>temp:
+        min_cnt=temp
+print(min_cnt)
