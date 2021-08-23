677e2403eeacca9f5dc2a15e6fc687a251c0093d
hojeong <jhj20071@naver.com>
2021-08-23 19:07:52 +0900

    스택 문제 import sys / sys.stdin.readline()으로 시간 초과 문제 해결

--- BEAKJOON/STUDY/day2/10828.py
+++ BEAKJOON/STUDY/day2/10828.py
@@ -1,21 +1,27 @@
 #스택
-#출력해야되는 명령어 pop,size,empty,top
-N=int(input())
+import sys
+N=int(sys.stdin.readline())
 stack=[]
 for i in range(N):
-    command=input()
-    if 'push' in command:
-        command=command[5:]
-        stack.append(command)
-    elif command=='pop' and stack:
-        print(stack.pop())
-    elif command=='size':
+    command=sys.stdin.readline().split()
+    word=command[0]
+    if word=='push':
+        value = command[1]
+        stack.append(value)
+    elif word == 'pop':
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack.pop())
+    elif word == 'size':
         print(len(stack))
-    elif command=='empty' and stack:
-        print(0)
-    elif command=='empty' and len(stack)==0:
-        print(1)
-    elif command=='top'and stack:
-        print(stack[-1])
-    elif len(stack)==0:
-        print(-1)
+    elif word == 'empty':
+        if len(stack)==0:
+            print(1)
+        else:
+            print(0)
+    elif word == 'top':
+        if len(stack)==0:
+            print(-1)
+        else:
+            print(stack[-1])
