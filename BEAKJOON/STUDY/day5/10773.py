fb7aa15ee6c4477a9c92fc54e9ecca18b6af4991
hojeong <jhj20071@naver.com>
2021-08-31 22:07:41 +0900

    제로 문제 완료

--- BEAKJOON/STUDY/day5/10773.py
+++ BEAKJOON/STUDY/day5/10773.py
@@ -1,1 +1,12 @@
 # 제로
+import sys
+K=int(input())
+stack=[]
+for i in range(K):
+    n=int(sys.stdin.readline())
+    if n!=0:
+        stack.append(n)
+    else:
+        stack.pop()
+ans=sum(stack)
+print(ans)
