0a117cb39a62e8ea65779cb94a8dfcf26fdf7795
hojeong <jhj20071@naver.com>
2021-09-01 00:23:59 +0900

    수정렬하기2 문제완료

--- BEAKJOON/CLASS2/2751.py
+++ BEAKJOON/CLASS2/2751.py
@@ -1,27 +1,7 @@
 # 수 정렬하기2
-def myMax(*args):
-    num=args[0]
-    for i in args:
-        if num<i:
-            num=i
-    return num
-
-N = int(input())
-n_list = [int(input()) for _ in range(N)]
-counting = [0] * (myMax(*n_list) + 1)*2
-
-for i in n_list:
-    if i<0:
-        counting[-i] += 1
-    else:
-        counting[i]+=1
-for i in range(len(counting)):
-    if counting[i]:
-        print(i)
-
-# 버블정렬
-# for i in range(len(n_list)-1,0,-1):
-#     for j in range(1,i+1):
-#         if n_list[j]<n_list[j-1]:
-#             n_list[j],n_list[j-1]=n_list[j-1],n_list[j]
-# print(*n_list,sep='\n')
+import sys
+N=int(input())
+n=[int(sys.stdin.readline()) for _ in range(N)]
+n.sort()
+for i in n:
+    print(i)
