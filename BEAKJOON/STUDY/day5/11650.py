bded0a061d8abd0690c3ccdd7c3c48394cedfb02
hojeong <jhj20071@naver.com>
2021-08-31 22:00:08 +0900

    좌표 정렬하기 문제 완료

--- BEAKJOON/STUDY/day5/11650.py
+++ BEAKJOON/STUDY/day5/11650.py
@@ -1,1 +1,8 @@
+import sys
+
 # 좌표 정렬하기
+N = int(input())  # 점의 개수
+n = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+n.sort(key=lambda x: (x[0], x[1]))  # 람다 이용해서 정렬
+for i in range(N):
+    print(*n[i])
