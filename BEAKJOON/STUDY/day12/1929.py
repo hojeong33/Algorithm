f09fa601f37f1a80f7b8a585ed32dd16abbd3423
hojeong <jhj20071@naver.com>
2021-10-05 22:49:31 +0900

    소수 구하기 시간초과 문제 해결

--- BEAKJOON/STUDY/day12/1929.py
+++ BEAKJOON/STUDY/day12/1929.py
@@ -1,1 +1,15 @@
 # 소수 구하기
+def check(n):
+    if n==1:
+        return False
+    k=2
+    while k*k<=n:
+        if n%k==0:
+            return False
+        k+=1
+    return True
+
+M,N=map(int,input().split())
+for i in range(M,N+1):
+    if check(i):
+        print(i)
