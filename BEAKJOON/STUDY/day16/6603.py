c62544d82b489bf0b7e734836795f4d1bba1828c
hojeong <jhj20071@naver.com>
2021-10-28 09:16:02 +0900

    로또 문제 완료

--- BEAKJOON/STUDY/day16/6603.py
+++ BEAKJOON/STUDY/day16/6603.py
@@ -1,1 +1,13 @@
 # 로또
+from itertools import combinations
+while 1:
+    temp=list(map(int,input().split()))
+    k=temp.pop(0)
+    if k==0:
+        break
+    else:
+        combination=combinations(temp,6)
+        for i in combination:
+            print(*i)
+    print()
+
