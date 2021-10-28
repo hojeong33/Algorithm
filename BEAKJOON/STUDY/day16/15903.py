934f194992b733ac37fb6b17d34bcd6d669ba6f2
hojeong <jhj20071@naver.com>
2021-10-28 18:24:28 +0900

    카드 합체 놀이

--- BEAKJOON/STUDY/day16/15903.py
+++ BEAKJOON/STUDY/day16/15903.py
@@ -1,1 +1,12 @@
 # 카드 합체 놀이
+n, m = map(int, input().split())
+cards = list(map(int, input().split()))
+cnt = 0
+while cnt < m:
+    cnt += 1
+    cards.sort()
+    a = cards.pop(0)
+    b = cards.pop(0)
+    cards.append(a + b)
+    cards.append(a + b)
+print(sum(cards))
