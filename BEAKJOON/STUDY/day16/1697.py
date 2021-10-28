bda9c0f68a3958a8f1776103c5acdb17179e1366
hojeong <jhj20071@naver.com>
2021-10-28 19:48:16 +0900

    숨바꼭질 문제 완료

--- BEAKJOON/STUDY/day16/1697.py
+++ BEAKJOON/STUDY/day16/1697.py
@@ -1,1 +1,27 @@
 # 숨바꼭질
+from collections import deque
+
+
+def bfs():
+    q = deque([[N, 0]])
+    visited[N] = 1
+    while q:
+        t = q.popleft()
+        if t[0] == K:
+            return t[1]
+        for i in range(3):
+            if i == 0:
+                nt = t[0] + 1
+            elif i == 1:
+                nt = t[0] - 1
+            elif i == 2:
+                nt = t[0] * 2
+            if 0 <= nt <= 100000 and not visited[nt]:
+                visited[nt] = 1
+                q.append([nt, t[1] + 1])
+
+
+N, K = map(int, input().split())
+visited = [0] * 100000 * 2
+ans = bfs()
+print(ans)
