8dd87ae4ae8b4c1c713835363a526758364386e5
hojeong <jhj20071@naver.com>
2021-11-01 19:18:51 +0900

    프린터 문제 완료 주석 추가

--- PROGRAMMERS/스택_큐/프린터.py
+++ PROGRAMMERS/스택_큐/프린터.py
@@ -3,17 +3,17 @@ def solution(priorities, location):
     while len(priorities) != 0:
         MAX = max(priorities)
         temp = priorities.pop(0)
-        if temp >= MAX:
-            answer += 1
-            if location == 0:
+        if temp >= MAX:  # 해당 값이 우선순위가 가장 클 때
+            answer += 1  # 인쇄
+            if location == 0:  # 찾는 인덱스면 멈춤
                 break
             else:
+                location -= 1  # 아니면 -1 (왼쪽으로 이동)
+        else:  # 우선순위가 최대가 아닐 때
+            priorities.append(temp)  # 마지막에 넣기
+            if location:  # location이 0이 아니라면
                 location -= 1
-        else:
-            priorities.append(temp)
-            if location:
-                location -= 1
-            else:
-                location = len(priorities) - 1
+            else:  # 0이라면
+                location = len(priorities) - 1  # 마지막 인덱스로
 
     return answer
