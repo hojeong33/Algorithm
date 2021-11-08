35ccebb2ad99d054eb4f16e122007961f9cb9216
hojeong <jhj20071@naver.com>
2021-11-08 19:28:15 +0900

    카펫 문제 완료

---
+++ PROGRAMMERS/완전탐색/카펫.py
@@ -0,0 +1,12 @@
+def solution(brown, yellow):
+    # 가로,세로 최소 길이는 3
+    for i in range(3, brown // 2):  # 한 변을 정하고
+        j = (brown + 4 - i * 2) // 2  # 나머지 변은 자동으로 정해짐
+        if (i - 2) * (j - 2) == yellow:
+            a = i
+            b = j
+            check = True
+            break
+    ans = [a, b]
+    ans.sort(reverse=True)  # 가로>=세로
+    return ans
