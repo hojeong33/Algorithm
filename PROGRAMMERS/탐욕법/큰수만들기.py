5aeb54f07f0b55e5b0fc2f86f6f52c6ea74b859d
hojeong <jhj20071@naver.com>
2021-11-11 00:06:29 +0900

    큰수만들기 완탐 -> 시간초과

--- PROGRAMMERS/탐욕법/큰수만들기.py
+++ PROGRAMMERS/탐욕법/큰수만들기.py
@@ -1,4 +1,13 @@
-number=['1924']
-k=2
-temp=sorted(number[0],reverse=True)
-print(''.join(temp[:k]))
+from itertools import combinations
+def solution(number, k):
+    combination=combinations(range(len(number)),k)
+    temps=[]
+    for i in combination:
+        temp=list(number)
+        for j in range(k-1,-1,-1):
+            temp.pop(i[j])
+        num=''
+        for j in temp:
+            num+=j
+        temps.append(num)
+    return max(temps)
