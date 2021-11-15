96481cfc59e4911dc70a234b6d925c7ff620fb64
hojeong <jhj20071@naver.com>
2021-11-15 18:40:30 +0900

    구멍보트 break -> 정확성 통과, 효율성 시간초과

--- PROGRAMMERS/탐욕법/구명보트.py
+++ PROGRAMMERS/탐욕법/구명보트.py
@@ -1,16 +1,14 @@
-from collections import deque
 def solution(people, limit):
     people.sort(reverse=True)
     cnt=0
-    # people=deque(people)
+    idx=1
     while len(people)>1:
         temp=people.pop(0)
         for i in range(len(people)):
             if temp+people[i]<=limit:
                 people.pop(i)
+                break
         cnt+=1
     if people:
         cnt+=1
     return cnt
-
-print(solution([40,50,70,80],100))
