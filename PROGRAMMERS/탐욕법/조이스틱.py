1aec19e8634470348f93c9028dc983489dd6309a
hojeong <jhj20071@naver.com>
2021-11-15 18:53:32 +0900

    조이스틱 테스트 11번 실패 (방향바꾸는 조건 추가)

--- PROGRAMMERS/탐욕법/조이스틱.py
+++ PROGRAMMERS/탐욕법/조이스틱.py
@@ -1,24 +1,23 @@
-name="JEROEN"
-check=[False]*len(name)
-check_num=0
-for i in range(len(name)):
-    if name[i]=='A':
-        check[i]=True
-        check_num+=1
-idx=0
-cnt=0
-d=1
-while 1:
-    if check[idx]==False:
-        temp=abs(ord('A')-ord(name[idx]))
-        cnt+=min(temp,26-temp)
-        check[idx]=True
-        check_num+=1
-    if check_num==len(name):
-        break
-    if check[idx+d]:
-        d*=-1
-    idx+=d
-    print(idx)
-    cnt+=1
-print(cnt)
+def solution(name):
+    check=[False]*len(name)
+    check_num=0
+    for i in range(len(name)):
+        if name[i]=='A':
+            check[i]=True
+            check_num+=1
+    idx=0
+    cnt=0
+    d=1
+    while 1:
+        if check[idx]==False:
+            temp=abs(ord('A')-ord(name[idx]))
+            cnt+=min(temp,26-temp)
+            check[idx]=True
+            check_num+=1
+        if check_num==len(name):
+            break
+        if check[idx+d] and check[idx-d]==False:
+            d*=-1
+        idx+=d
+        cnt+=1
+    return cnt
