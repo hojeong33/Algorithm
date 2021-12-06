015ea54f4892f910140d344baaac40168ef8e431
hojeong <jhj20071@naver.com>
2021-12-06 19:46:19 +0900

    여행경로 deepcopy 사용 문제 해결

--- PROGRAMMERS/깊이너비우선탐색/여행경로.py
+++ PROGRAMMERS/깊이너비우선탐색/여행경로.py
@@ -1,21 +1,22 @@
 from collections import deque
-tickets=[["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
+from copy import deepcopy
+# tickets=[["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
+tickets=[["ICN", "B"], ["B", "ICN"], ["ICN", "A"], ["A", "D"], ["D", "A"]]
+# ["ICN", "B", "ICN", "A", "D", "A"]
 tickets.sort(key=lambda x:x[1])
-print(tickets)
 def bfs():
-    q=["ICN"]
-    route=["ICN"]
-    q=deque(q)
-    l=len(tickets)
-    visited=[0 for _ in range(l)]
+    q=deque()
+    l = len(tickets)
+    q.append(("ICN",["ICN"],[0 for _ in range(l)]))
     while q:
-        t=q.popleft()
+        dst,route,visited = q.popleft()
+        if len(route)==l+1:
+            print(route)
         for i in range(l):
-            if tickets[i][0]==t and not visited[i]:
-                visited[i]=1
-                q.append(tickets[i][1])
-                route.append(tickets[i][1])
-                break
-    print(route)
+            if tickets[i][0] == dst and not visited[i]:
+                visited[i] = 1
+                newvisited=deepcopy(visited)
+                q.append((tickets[i][1],route+[tickets[i][1]],newvisited))
+                visited[i]=0
 bfs()
 
