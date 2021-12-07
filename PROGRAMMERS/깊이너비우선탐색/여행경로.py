345bce6ff8a221aab48dde821f1321cf92b24057
hojeong <jhj20071@naver.com>
2021-12-07 14:15:56 +0900

    여행경로 문제 완료

--- PROGRAMMERS/깊이너비우선탐색/여행경로.py
+++ PROGRAMMERS/깊이너비우선탐색/여행경로.py
@@ -1,22 +1,25 @@
 from collections import deque
 from copy import deepcopy
-# tickets=[["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
-tickets=[["ICN", "B"], ["B", "ICN"], ["ICN", "A"], ["A", "D"], ["D", "A"]]
-# ["ICN", "B", "ICN", "A", "D", "A"]
-tickets.sort(key=lambda x:x[1])
-def bfs():
-    q=deque()
-    l = len(tickets)
-    q.append(("ICN",["ICN"],[0 for _ in range(l)]))
-    while q:
-        dst,route,visited = q.popleft()
-        if len(route)==l+1:
-            print(route)
-        for i in range(l):
-            if tickets[i][0] == dst and not visited[i]:
-                visited[i] = 1
-                newvisited=deepcopy(visited)
-                q.append((tickets[i][1],route+[tickets[i][1]],newvisited))
-                visited[i]=0
-bfs()
+
+
+def solution(tickets):
+    tickets.sort(key=lambda x: x[1])  # 오름차순 정렬
+
+    def bfs():
+        q = deque()
+        l = len(tickets)
+        q.append(("ICN", ["ICN"], [0 for _ in range(l)]))  # 출발지, 경로, 방문
+        while q:
+            dst, route, visited = q.popleft()
+            if len(route) == l + 1:  # 경로가 티켓길이 +1 이면 경로 반환
+                return route
+            for i in range(l):
+                if tickets[i][0] == dst and not visited[i]:  # 출발지가 같고 방문하지 않았으면
+                    visited[i] = 1  # 방문체크
+                    newvisited = deepcopy(visited)
+                    q.append((tickets[i][1], route + [tickets[i][1]], newvisited))
+                    visited[i] = 0  # 방문해제
+
+    answer = bfs()
+    return answer
 
