76c72937bceb179198143af7d19cb02314cfcb0d
hojeong <jhj20071@naver.com>
2021-12-16 20:58:09 +0900

    전화번호목록 문제 완료 문자열 정렬

--- PROGRAMMERS/해시/전화번호목록.py
+++ PROGRAMMERS/해시/전화번호목록.py
@@ -1,6 +1,8 @@
 def solution(phone_book):
     answer=True
     phone_book.sort()
+    #문자열 정렬 '12','123','13' -> '12','123','13'
+    #숫자 정렬 12,123,13-> 12,13,123
     for i in range(len(phone_book)-1):
         l_1=len(phone_book[i])
         l_2=len(phone_book[i+1])
