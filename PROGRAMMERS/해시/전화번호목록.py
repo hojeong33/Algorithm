40e4a0d4874cc81f2d545c7dd1fae1dbe4fa8faf
hojeong <jhj20071@naver.com>
2021-12-16 20:56:20 +0900

    전화번호목록 문제 완료

--- PROGRAMMERS/해시/전화번호목록.py
+++ PROGRAMMERS/해시/전화번호목록.py
@@ -1,13 +1,13 @@
-phone_book=["119", "97674223", "1195524421"]
-answer=True
-for i in range(len(phone_book)-1):
-    l_1=len(phone_book[i])
-    for j in range(i+1,len(phone_book)):
-      l_2=len(phone_book[j])
-      if l_1<l_2 and  phone_book[i]==phone_book[j][:l_1]:
-          answer=False
-      elif phone_book[i][:l_2]==phone_book[j]:
-          answer=False
-    if not answer:
-        break
-print(answer)
+def solution(phone_book):
+    answer=True
+    phone_book.sort()
+    for i in range(len(phone_book)-1):
+        l_1=len(phone_book[i])
+        l_2=len(phone_book[i+1])
+        if l_1<l_2 and  phone_book[i]==phone_book[i+1][:l_1]:
+            answer=False
+        elif phone_book[i][:l_2]==phone_book[i+1]:
+            answer=False
+        if not answer:
+            break
+    return answer
