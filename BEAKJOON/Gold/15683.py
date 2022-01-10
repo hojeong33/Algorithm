64192eda995d797c28b80042926d777c7aed2d59
hojeong <jhj20071@naver.com>
2022-01-10 21:09:21 +0900

    감시 테케 통과 40%틀림

--- BEAKJOON/Gold/15683.py
+++ BEAKJOON/Gold/15683.py
@@ -1,1 +1,77 @@
 #감시
+from copy import deepcopy
+from collections import deque
+
+dr=[-1,0,1,0]
+dc=[0,1,0,-1]
+
+def check(cnt):
+    global ans
+    if cnt==cctv:
+        tmp=0
+        for i in range(N):
+            for j in range(M):
+                if not bang2[i][j]:
+                    tmp+=1
+        # print(bang2,tmp)
+        return tmp
+    r,c=cc[cnt][0],cc[cnt][1]
+    for d in range(4):
+        directions = []
+        if bang[r][c]==1:
+            directions.append(d)
+        elif bang[r][c]==2:
+            directions.append(d)
+            directions.append((d+2)%4)
+        elif bang[r][c]==3:
+            directions.append(d)
+            directions.append((d-1)%4)
+        elif bang[r][c]==4:
+            directions.append(d)
+            directions.append((d -1) % 4)
+            directions.append((d + 2) % 4)
+        elif bang[r][c]==5:
+            directions.append(d)
+            directions.append((d-1)%4)
+            directions.append((d+1)%4)
+            directions.append((d+2)%4)
+        q=deque()
+        for k in directions:
+            nr,nc=r+dr[k],c+dc[k]
+            while 0<=nr<N and 0<=nc<M:
+                if bang2[nr][nc]==6:
+                    break
+                elif not bang2[nr][nc]:
+                    bang2[nr][nc]=1
+                    q.append((nr,nc))
+                nr+=dr[k]
+                nc+=dc[k]
+        ans=min(ans,check(cnt+1))
+        while q:
+            pr,pc=q.popleft()
+            if not bang[pr][pc]:
+                bang2[pr][pc]=0
+        if bang[r][c]==5:
+            break
+    return ans
+
+#입력
+N,M=map(int,input().split())
+bang=[[]for _ in range(N)]
+for i in range(N):
+    bang[i]=list(map(int,input().split()))
+
+# cctv 개수
+cctv=0
+cc=[]
+for i in range(N):
+    for j in range(M):
+        if 0<bang[i][j]<6:
+            cctv+=1
+            cc.append((i,j))
+ans= 987654321 #사각지대 초깃값
+bang2=deepcopy(bang)
+check(0)
+print(ans)
+
+
--- BEAKJOON/Gold/17144.py
+++ BEAKJOON/Gold/17144.py
@@ -1,4 +1,5 @@
 #미세먼지안녕
+from copy import deepcopy
 # 입력
 from collections import deque
 R,C,T=map(int,input().split()) # R:행, C:열, T:초
@@ -6,11 +7,18 @@ room=[[]for _ in range(R)]
 for i in range(R):
     room[i]=list(map(int,input().split()))
 
+m=[]
+for i in range(R):
+    for j in range(C):
+        if room[i][j]==-1:
+            m.append(i)
+
 #확산
 
-dr=[1,0,-1,0]
-dc=[0,-1,0,1]
+dr=[0,-1,0,1]
+dc=[1,0,-1,0]
 check=[[0]*C for _ in range(R)]
+# check2=[[0]*C for _ in range(R)]
 
 def bfs(r,c):
     q=deque()
@@ -31,13 +39,57 @@ def bfs(r,c):
             check[nr][nc]+=a
         room[r][c]-=cnt*a
 
+while T:
+    for i in range(R):
+        for j in range(C):
+            if room[i][j]>0:
+                bfs(i,j)
+    for i in range(R):
+        for j in range(C):
+            room[i][j]+=check[i][j]
+    print(room)
 
+
+    # 공기청정기 작동
+    tmp=deepcopy(room)
+    m1=m[0]
+    m2=m[1]
+    # print(m1,m2)
+    for c in range(2,C-1):
+        tmp[m1][c]=room[m1][c-1]
+        tmp[0][c]=room[0][c+1]
+        tmp[m2][c]=room[m2][c-1]
+        tmp[R-1][c]=room[R-1][c+1]
+        # print(tmp)
+    for r in range(0,m1):
+        tmp[r][C-1]=room[r+1][C-1]
+        tmp[r][0]=room[r-1][0]
+        # print(tmp)
+    for r in range(m2+1,R-1):
+        tmp[r][C-1]=room[r-1][C-1]
+        tmp[r][0]=room[r+1][0]
+        # print(tmp)
+
+    tmp[m1][1]=0
+    tmp[m2][1]=0
+
+    tmp[m1][C-1]=room[m1][C-2]
+    tmp[m2][C-1]=room[m2][C-2]
+
+    tmp[0][C-1]=room[1][C-1]
+    tmp[0][1]=room[0][2]
+    tmp[0][2]=room[0][1]
+
+    tmp[R-1][C-1]=room[R-2][C-1]
+    tmp[R-1][1]=room[R-1][2]
+    tmp[R-1][0]=room[R-1][1]
+
+    print(tmp)
+    room=deepcopy(tmp)
+    T-=1
+ans=0
 for i in range(R):
     for j in range(C):
-        if room[i][j]>0:
-            bfs(i,j)
-for i in range(R):
-    for j in range(C):
-        room[i][j]+=check[i][j]
-print(room)
-
+        if tmp[i][j]!=-1:
+            ans+=tmp[i][j]
+print(ans)
