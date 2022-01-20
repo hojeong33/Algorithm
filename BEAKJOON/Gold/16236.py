6f446aa8e16c8dbe23d7a7eba6b84fec0530d8c4
hojeong <jhj20071@naver.com>
2022-01-20 20:47:34 +0900

    아기 상어 완료

--- BEAKJOON/Gold/16236.py
+++ BEAKJOON/Gold/16236.py
@@ -1,60 +1,58 @@
 #아기 상어
-
-
 from collections import deque
 
-n = int(input())
+dr=[-1,0,1,0]
+dc=[0,1,0,-1]
 
-arr = [list(map(int, input().split())) for _ in range(n)]
+#입력
+N=int(input())
+arr=[list(map(int,input().split()))for _ in range(N)]
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
 
-sx, sy = 0, 0
-for i in range(n):
-    for j in range(n):
-        if arr[i][j] == 9:
-            arr[i][j] = 0
-            sx, sy = i, j
+# 상어 위치 찾기
+for r in range(N):
+    for c in range(N):
+        if arr[r][c]==9:
+            start=[r,c]
+            arr[r][c]=0
             break
-size = 2
-move_num = 0
-eat = 0
-while True:
-    q = deque()
-    q.append((sx, sy, 0))
-    visited = [[False] * n for _ in range(n)]
-    flag = 1e9
-    fish = []
+
+ans=0 # 시간
+size=2 # 가장 처음의 아기 상어의 크기
+eat=0 # 물고기 먹은 수
+
+while 1:
+    q=deque()
+    q.append((start[0],start[1],0)) # 상어 r, 상어 c, 이동횟수
+    visited=[[False]*N for _ in range(N)]
+    flag=987654321
+    fish=[] # 먹을 수 있는 물고기
     while q:
-        x, y, count = q.popleft()
-
-        if count > flag:
+        r,c,cnt=q.popleft()
+        if cnt>flag:
             break
-        for k in range(4):
-            nx, ny = x + dx[k], y + dy[k]
-            if nx < 0 or ny < 0 or nx >= n or ny >= n:
+        for d in range(4):
+            nr,nc=r+dr[d],c+dc[d]
+            if nr<0 or nr>=N or nc<0 or nc>=N: # 범위체크
                 continue
-            if arr[nx][ny] > size or visited[nx][ny]:
+            if arr[nr][nc]>size or visited[nr][nc]: #사이즈, 방문 체크
                 continue
+            if 0<arr[nr][nc]<size: # 상어보다 사이즈 작은 물고기 이면
+                fish.append((cnt+1,nr,nc))
+                flag=cnt
+            visited[nr][nc]=True #방문체크
+            q.append((nr,nc,cnt+1))
 
-            if arr[nx][ny] != 0 and arr[nx][ny] < size:
-                fish.append((nx, ny, count + 1))
-                flag = count
-            visited[nx][ny] = True
-            q.append((nx, ny, count + 1))
-
-    if len(fish) > 0:
+    if len(fish)>0:
         fish.sort()
-        x, y, move = fish[0][0], fish[0][1], fish[0][2]
-        move_num += move
-        eat += 1
-        arr[x][y] = 0
-        if eat == size:
-            size += 1
-            eat = 0
-        sx, sy = x, y
+        m,r,c=fish[0][0],fish[0][1],fish[0][2]
+        ans+=m
+        eat+=1
+        arr[r][c]=0
+        if eat==size:
+            size+=1
+            eat=0
+        start=[r,c]
     else:
         break
-
-print(move_num)
+print(ans)
--- BEAKJOON/Gold/17140.py
+++ BEAKJOON/Gold/17140.py
@@ -1,1 +1,10 @@
 #이차원 배열과 연산
+a=[[1,2],[2,3],[2,1],[1,3]]
+a.sort()
+print(a)
+a.sort(key=lambda x:x[1])
+print(a)
+a.sort(key=lambda x:x[0])
+print(a)
+a.sort(key=lambda x:(x[1],x[0]))
+print(a)
