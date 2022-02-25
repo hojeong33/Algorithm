2406c04c20c2bfdb444cfe6e83d2e98df5dc4392
hojeong <jhj20071@naver.com>
2022-02-25 12:31:17 +0900

    탑 시간초과-> 스택으로 풀기

--- BEAKJOON/Gold/2493_탑.py
+++ BEAKJOON/Gold/2493_탑.py
@@ -1,18 +1,39 @@
 #탑
-def find_top(i):
-    temp = tops[i]  # 탑 높이
-    while 1:  # 송신 가장 먼저 닫는 곳 찾기
-        for j in range(i - 1, 0, -1):
-            if temp < tops[j]:
-                ans[i] = j + 1
-                return
-        else:
-            ans[i]=0
-            return
+N = int(input())  # 탑의 개수
+tops = list(map(int, input().split()))
+stack = []
+ans = []
 
-N=int(input())
-tops=list(map(int,input().split()))
-ans=[0]*N
-for i in range(N - 1, 0, -1):
-    find_top(i)
+for i in range(N):
+    while stack:
+        if stack[-1][1] > tops[i]:
+            ans.append(stack[-1][0] + 1)
+            break
+        else:
+            stack.pop()
+    if not stack:
+        ans.append(0)
+    stack.append([i, tops[i]])
+
 print(*ans)
+
+
+
+
+
+
+# 시간초과 -> 스택으로 풀기
+# def find_top(i):
+#     temp = tops[i]  # 탑 높이
+#     for j in range(i-1, -1, -1):
+#         if temp < tops[j]:
+#             ans[i] = j + 1
+#             return
+#
+# N=int(input())
+# tops=list(map(int,input().split()))
+# ans=[0]*N
+#
+# for i in range(N - 1, 0, -1):
+#     find_top(i)
+# print(*ans)
